a6cddbd036c655c98224a5e62d2a91d7355c2216
kennethsim2000 <e0725254@u.nus.edu>
2022-10-13 23:38:35 +0800

    Complete pern todolist

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -5,9 +5,32 @@ import Button from 'react-bootstrap/Button';
 /*EditTodo is rendered in the ListTodo class */
 const EditTodo = ({ todo }) => {
   const [show, setShow] = useState(false);
+  const [description, setDescription] = useState(todo.description);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = () => {
+    setDescription(todo.description);
+    setShow(true);
+  }
+
+  //edit description function
+
+  const updateDescription = async e =>{
+    e.preventDefault();
+    try{
+        const body = { description };
+        const response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`,{
+            method: "PUT",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(body)
+        })
+
+        window.location = "/"; //this refreshes the window after the update
+
+    } catch(err) {
+        console.error(err.message);
+    }
+  }
     return (
         <Fragment>
             <Button variant="warning" onClick={handleShow}>
@@ -18,9 +41,12 @@ const EditTodo = ({ todo }) => {
                 <Modal.Header closeButton>
                 <Modal.Title>Edit Todo</Modal.Title>
                 </Modal.Header>
-                <Modal.Body><input type="text" className="form-control"/></Modal.Body>
+                <Modal.Body>
+                    <input type="text" className="form-control" value={description} 
+                    onChange={e=>setDescription(e.target.value)}/>
+                </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="warning" onClick={handleClose}>
+                    <Button variant="warning" onClick={e => updateDescription(e)}>
                         Save Changes
                     </Button>
                     <Button variant="primary" onClick={handleClose}>
